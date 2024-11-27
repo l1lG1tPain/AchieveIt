1a11d30e83aab2777749242c215054de9e64424e
l1lG1tPain <project.xype@mail.ru>
2024-11-27 23:08:16 +0500

    add-manifest

--- script.js
+++ script.js
@@ -179,3 +179,14 @@ function closeInstallSheet() {
 // Initialize
 renderDates();
 renderTasks();
+
+// Регистрация Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((registration) => {
+      console.log('Service Worker успешно зарегистрирован:', registration);
+    })
+    .catch((error) => {
+      console.error('Ошибка регистрации Service Worker:', error);
+    });
+}
